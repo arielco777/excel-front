72269de720ab648f65ecca1fd07d335515872ad9
Ariel Cohen <arielco777@gmail.com>
2024-05-24 15:13:27 -0400

    something else

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Outlet } from "react-router-dom";
+// Some Comment
 const App = () => {
-   return (
-        <div className= "flex flex-col h-screen">
+    return (
+        <div className="flex h-screen flex-col">
             {/* <Navbar /> */}
             <Outlet />
         </div>
